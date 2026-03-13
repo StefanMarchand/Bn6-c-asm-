5c626fdc6efb6d73f5f8e867ea9cec43bd1a5e65
LanClaw <marchandstefan@gmail.com>
2026-03-13 00:05:01 -0500

    decomp(openrouter-healer-alpha-1773378240): ExpoSite_LoadGFXAnims

--- src/ExpoSite_LoadGFXAnims.c
+++ src/ExpoSite_LoadGFXAnims.c
@@ -1,15 +1,8 @@
 ```c
-#include "global.h"
-#include "event_data.h"
-#include "script.h"
-
-static void ExpoSite_LoadGFXAnimsInternal(void);
+#include "maps/groups.h"
+#include "maps/loader.h"
 
 void ExpoSite_LoadGFXAnims(void) {
-    ExpoSite_LoadGFXAnimsInternal();
-}
-
-static void ExpoSite_LoadGFXAnimsInternal(void) {
-    LoadGFXAnimsForMapGroup(MAP_GROUP_EXPO_SITE, MAP_NUM_EXPO_SITE);
+    LoadMapGFXAnims(MAP_GROUP_EXPO_SITE);
 }
 ```
