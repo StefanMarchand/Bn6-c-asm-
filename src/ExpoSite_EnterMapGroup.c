b8e80fc020e109e950d4aee15d2b4df741c9810e
LanClaw <marchandstefan@gmail.com>
2026-03-13 01:44:38 -0500

    decomp(openrouter-hunter-alpha-1773384113): ExpoSite_EnterMapGroup

--- src/ExpoSite_EnterMapGroup.c
+++ src/ExpoSite_EnterMapGroup.c
@@ -2,23 +2,11 @@
 #include "global.h"
 #include "map_group.h"
 
-// Function: ExpoSite_EnterMapGroup
-// Address: 0x08062AB0
-// Purpose: Map group entry loader for asm09 Expo Site area
-static void ExpoSite_LoadTilesets(void);
-
-void ExpoSite_EnterMapGroup(void)
-{
-    ExpoSite_LoadTilesets();
-    SetMapGroupCallbacks(&ExpoSite_MapGroupCallbacks);
-    LoadMapGroupEvents(ExpoSite_Events);
-    SetMapGroupEncounters(&ExpoSite_Encounters);
-}
-
-static void ExpoSite_LoadTilesets(void)
-{
-    LoadPrimaryTileset(&gTileset_ExpoSite);
-    LoadSecondaryTileset(&gTileset_ExpoSite_S);
-    ApplyTilesetPalettes(&gTileset_ExpoSite);
+static void ExpoSite_EnterMapGroup(void) {
+    // Map group entry loader for asm09 Expo Site area
+    // Address: 0x08062AB0
+    
+    LoadMapGroup(0x09);  // MAP_GROUP_EXPO_SITE = 0x09
+    SetCurrentMapGroup(0x09);
 }
 ```
