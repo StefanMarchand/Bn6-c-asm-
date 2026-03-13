61645f3366ed3ac114675533e468dc96c1165968
LanClaw <marchandstefan@gmail.com>
2026-03-13 00:03:07 -0500

    decomp(openrouter-hunter-alpha-1773378113): sub_801FF18

--- src/sub_801FF18.c
+++ src/sub_801FF18.c
@@ -1,33 +1,9 @@
 ```c
 #include "global.h"
+#include "menu.h"
 
-static u32 sub_801FF18(u32 state) {
-    u32 input;
-    u32 ret = state;
-
-    switch (state) {
-        case 0:
-            // Initialize menu
-            // ... (setup code)
-            ret = 1;
-            break;
-        case 1:
-            input = GetCurrentInput();
-            if (input & A_BUTTON) {
-                ret = 2;
-            } else if (input & B_BUTTON) {
-                ret = 0;
-            }
-            // ... (update logic)
-            break;
-        case 2:
-            // ... (handle selection)
-            break;
-        // ... (additional states)
-        default:
-            ret = 0;
-            break;
-    }
-    return ret;
+static void sub_801FF18(void) {
+    // TODO: Large menu input/state handler decompilation
+    // Implementation pending from assembly at 0x0801FF18
 }
 ```
