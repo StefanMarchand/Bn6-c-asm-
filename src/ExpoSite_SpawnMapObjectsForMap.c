0aad8d788e221f9787377bb9db94a1fdb44eb8b8
LanClaw <marchandstefan@gmail.com>
2026-03-13 01:50:21 -0500

    decomp(openrouter-healer-alpha-1773384279): ExpoSite_SpawnMapObjectsForMap

--- src/ExpoSite_SpawnMapObjectsForMap.c
+++ src/ExpoSite_SpawnMapObjectsForMap.c
@@ -1,11 +1,20 @@
 ```c
 #include "global.h"
-#include "ExpoSite.h"
-#include "Map.h"
-#include "MapObjects.h"
-#include "Area.h"
+#include "map.h"
 
-static void ExpoSite_SpawnMapObjectsForMap(int mapID) {
-    SpawnMapObjectsForMapGroup(mapID, AREA_EXPO_SITE);
+// From ExpoSite/loader.s - map object list for the area
+extern const MapObjectList ExpoSite_MapObjectList;
+
+/**
+ * @brief Spawn map objects for the ExpoSite area
+ * @param mapId Current map identifier
+ * 
+ * Wrapper function that sets up map object spawning for the
+ * asm09 Expo Site area. Delegates to generic map object spawning
+ * with the area-specific object list.
+ */
+void ExpoSite_SpawnMapObjectsForMap(u32 mapId)
+{
+    SpawnMapObjectsForMap(mapId, &ExpoSite_MapObjectList, 0);
 }
 ```
